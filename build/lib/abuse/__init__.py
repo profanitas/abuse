54f0dfc77b406c6d310237bed3698726408668e3
martalais <martalaismd@gmail.com>
2018-10-01 16:50:13 -0300

    Add nicknames functions

--- build/lib/abuse/__init__.py
+++ build/lib/abuse/__init__.py
@@ -1,8 +1,11 @@
 import random
 
 dataset=['anus', 'arse', 'arsehole', 'ass', 'ass-hat', 'ass-jabber', 'ass-pirate', 'assbag', 'assbandit', 'assbanger', 'assbite', 'assclown', 'asscock', 'asscracker', 'asses', 'assface', 'assfuck', 'assfucker', 'assgoblin', 'asshat', 'asshead', 'asshole', 'asshopper', 'assjacker', 'asslick', 'asslicker', 'assmonkey', 'assmunch', 'assmuncher', 'assnigger', 'asspirate', 'assshit', 'assshole', 'asssucker', 'asswad', 'asswipe', 'axwound', 'bampot', 'bastard', 'beaner', 'bitch', 'bitchass', 'bitches', 'bitchtits', 'bitchy', 'blow job', 'blowjob', 'bollocks', 'bollox', 'boner', 'brotherfucker', 'bullshit', 'bumblefuck', 'butt plug', 'butt-pirate', 'buttfucka', 'buttfucker', 'camel toe', 'carpetmuncher', 'chesticle', 'chinc', 'chink', 'choad', 'chode', 'clit', 'clitface', 'clitfuck', 'clusterfuck', 'cock', 'cockass', 'cockbite', 'cockburger', 'cockface', 'cockfucker', 'cockhead', 'cockjockey', 'cockknoker', 'cockmaster', 'cockmongler', 'cockmongruel', 'cockmonkey', 'cockmuncher', 'cocknose', 'cocknugget', 'cockshit', 'cocksmith', 'cocksmoke', 'cocksmoker', 'cocksniffer', 'cocksucker', 'cockwaffle', 'coochie', 'coochy', 'coon', 'cooter', 'cracker', 'cum', 'cumbubble', 'cumdumpster', 'cumguzzler', 'cumjockey', 'cumslut', 'cumtart', 'cunnie', 'cunnilingus', 'cunt', 'cuntass', 'cuntface', 'cunthole', 'cuntlicker', 'cuntrag', 'cuntslut', 'dago', 'damn', 'deggo', 'dick', 'dick-sneeze', 'dickbag', 'dickbeaters', 'dickface', 'dickfuck', 'dickfucker', 'dickhead', 'dickhole', 'dickjuice', 'dickmilk', 'dickmonger', 'dicks', 'dickslap', 'dicksucker', 'dicksucking', 'dicktickler', 'dickwad', 'dickweasel', 'dickweed', 'dickwod', 'dike', 'dildo', 'dipshit', 'doochbag', 'dookie', 'douche', 'douche-fag', 'douchebag', 'douchewaffle', 'dumass', 'dumb ass', 'dumbass', 'dumbfuck', 'dumbshit', 'dumshit', 'dyke', 'fag', 'fagbag', 'fagfucker', 'faggit', 'faggot', 'faggotcock', 'fagtard', 'fatass', 'fellatio', 'feltch', 'flamer', 'fuck', 'fuckass', 'fuckbag', 'fuckboy', 'fuckbrain', 'fuckbutt', 'fuckbutter', 'fucked', 'fucker', 'fuckersucker', 'fuckface', 'fuckhead', 'fuckhole', 'fuckin', 'fucking', 'fucknut', 'fucknutt', 'fuckoff', 'fucks', 'fuckstick', 'fucktard', 'fucktart', 'fuckup', 'fuckwad', 'fuckwit', 'fuckwitt', 'fudgepacker', 'gay', 'gayass', 'gaybob', 'gaydo', 'gayfuck', 'gayfuckist', 'gaylord', 'gaytard', 'gaywad', 'goddamn', 'goddamnit', 'gooch', 'gook', 'gringo', 'guido', 'handjob', 'hard on', 'heeb', 'hell', 'hoe', 'homo', 'homodumbshit', 'honkey', 'humping', 'jackass', 'jagoff', 'jap', 'jerk off', 'jerkass', 'jigaboo', 'jizz', 'jungle bunny', 'junglebunny', 'kike', 'kooch', 'kootch', 'kraut', 'kunt', 'kyke', 'lameass', 'lardass', 'lesbian', 'lesbo', 'lezzie', 'mcfagget', 'mick', 'minge', 'mothafucka', "mothafuckin\\'", 'motherfucker', 'motherfucking', 'muff', 'muffdiver', 'munging', 'negro', 'nigaboo', 'nigga', 'nigger', 'niggers', 'niglet', 'nut sack', 'nutsack', 'paki', 'panooch', 'pecker', 'peckerhead', 'penis', 'penisbanger', 'penisfucker', 'penispuffer', 'piss', 'pissed', 'pissed off', 'pissflaps', 'polesmoker', 'pollock', 'poon', 'poonani', 'poonany', 'poontang', 'porch monkey', 'porchmonkey', 'prick', 'punanny', 'punta', 'pussies', 'pussy', 'pussylicking', 'puto', 'queef', 'queer', 'queerbait', 'queerhole', 'renob', 'rimjob', 'ruski', 'sand nigger', 'sandnigger', 'schlong', 'scrote', 'shit', 'shitass', 'shitbag', 'shitbagger', 'shitbrains', 'shitbreath', 'shitcanned', 'shitcunt', 'shitdick', 'shitface', 'shitfaced', 'shithead', 'shithole', 'shithouse', 'shitspitter', 'shitstain', 'shitter', 'shittiest', 'shitting', 'shitty', 'shiz', 'shiznit', 'skank', 'skeet', 'skullfuck', 'slut', 'slutbag', 'smeg', 'snatch', 'spic', 'spick', 'splooge', 'spook', 'suckass', 'tard', 'testicle', 'thundercunt', 'tit', 'titfuck', 'tits', 'tittyfuck', 'twat', 'twatlips', 'twats', 'twatwaffle', 'unclefucker', 'va-j-j', 'vag', 'vagina', 'vajayjay', 'vjayjay', 'wank', 'wankjob', 'wetback', 'whore', 'whorebag', 'whoreface', 'wop']
-#dataset created by scrapping https://www.noswearing.com/dictionary/ 
+#dataset created by scrapping https://www.noswearing.com/dictionary/
+dataset_nick = ["Fuckstick","Cockmuppet","Assclown","Douchemonger","Twat","Mouth-breather","Cockshiner","Cheesedick-Fuckface","Knuckle-dragger","Herb-Shitstick-Tool","Shitbag","Carpet-cleaner","Asshat","Slutbag","Numbnuts","Dicknose","Cum-dumpster","Weaksauce","Sleezebag","Buttmunch","Twatwaffle","Tard","Cunt-rag","Cuntmuscle","Shitstain","Dickbreath","Jizztissue","Cockgobbler","Cuntkicker","Douchenozzle","Pigfucker","Butknuckler","Clitsplitter","Shitshaker","Rumpleforeskin","Douche-canoe","Fuckrag","Rumpranger","Cock-juggling-thundercunt"]
+# initial dataset created by scrapping https://top-funny-jokes.com/funny-insulting-names/
 
+# Shows the list of abuses thats an
 def ListAbusesFrom(data_in):
 	_temp=[]
 	if type(data_in) is not str:
@@ -31,4 +34,38 @@ def ListAnyAbuse():
 	return random.choice(dataset)
 
 def ListAllAbuses():
-	return dataset
+	return datasetDEF
+
+
+# Shows the list of nicknames thats an
+def ListNickFrom(data_in):
+	_temp=[]
+	if type(data_in) is not str:
+		return("No Nicknames Words from", data_in, "Provide input between a-z instead")
+	data_in=str(data_in).lower()
+	for i in dataset_nick:
+		if i.startswith(data_in):
+			_temp.append(i)
+
+
+	return _temp
+
+def RandomNickFrom(data_in):
+	_temp=[]
+	if type(data_in) is not str:
+		return("No Nicknames Words from", data_in, "Provide input between a-z instead")
+	data_in=str(data_in).lower()
+	for i in dataset_nick:
+		if i.startswith(data_in):
+			_temp.append(i)
+
+	return random.choice(_temp)
+
+def ListAnyNicks():
+	return random.choice(dataset_nick)
+
+def ListAllNicks():
+	return dataset_nickDEF
+
+
+
